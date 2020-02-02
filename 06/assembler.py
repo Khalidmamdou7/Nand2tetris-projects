510cac65d3a853cc776c3b653d95dfe66e67f161
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2020-02-02 03:48:50 +0300

    Assembler Less symbols
    Assembler Program that works without symbols

--- 06/assembler.py
+++ 06/assembler.py
@@ -1,11 +1,12 @@
 # Assembler program that translates assembly programs to binary machine language 
-from sys 
+import sys
 
+from helpers import comptrans, desttrans, jumptrans
 
 def main():
 	if len(sys.argv) != 2:
 		print("Invalid format! Correct one: 'Assembler filename.asm' ")
-	else
+	else:
 		# Opening files to read from and to write into
 		argv_1 = sys.argv[1]
 		fasm = argv_1.split('.')
@@ -16,9 +17,51 @@ def main():
 		# Split each line in the file
 		lines = fr.read()
 		line = lines.splitlines()
+		lineslen = len(line)
 
-		
+		# Loop over each line
+		for x in line:
+			# skip lines with comments or white space
+			if x.startswith('//') or not x: 
+				continue
+			# if the line is a a-instruction 
+			if x.startswith('@'):
+				ainst = x.strip('@')
+				binary = bin(int(ainst)).lstrip('0b')
+				fw.write('0' * (16 - len(binary)) + f'{binary}\n')
 
+			# if the line is a label
+			elif x.startswith('('):
+				print('label')
+			# if line is C-instruction
+			else:
+				dest = comp = jump = None
+
+				# if dest exists
+				if '=' in x :
+					cinst = x.split('=')
+					dest = cinst[0]
+					# if jump exists
+					if ';' in x:
+						comp = cinst[1].split(';')[0]	
+						jump = cinst[1].split(';')[1]
+					else:
+						comp = cinst[1]
+				elif not '=' in x:
+					cinst = x.split(';')
+					comp = cinst[0]
+					jump = cinst[1]
+
+
+				# translation
+				comp = comptrans(comp)
+				dest = desttrans(dest)
+				jump = jumptrans(jump)
+
+				fw.write('111' + f'{comp}{dest}{jump}\n')
+
+
+				
 
 
 
---
+++ 06/helpers.py
@@ -0,0 +1,130 @@
+def comptrans(comp):
+	if comp == '0':
+		comp = '101010'
+		acomp = '0'
+	elif comp == '1':
+		comp = '111111'
+		acomp = '0'
+	elif comp == '-1':
+		comp = '111010'
+		acomp = '0'
+	elif comp =='D':
+		comp = '001100'
+		acomp = '0'
+	elif comp == 'A':
+		acomp = '0'
+		comp = '110000'
+	elif comp == 'M':
+		acomp = '1'
+		comp = '110000'
+	elif comp == '!D':
+		acomp = '0'
+		comp = '001101'
+	elif comp == '!A':
+		acomp = '0'
+		comp = '110001'
+	elif comp == '!M':
+		acomp = '1'
+		comp = '110001'
+	elif comp == '-D':
+		acomp = '0'
+		comp = '001111'
+	elif comp == '-A':
+		acomp = '0'
+		comp = '110011'
+	elif comp == '-M':
+		acomp = '1'
+		comp = '110011'
+	elif comp == 'D+1':
+		acomp = '0'
+		comp = '011111'
+	elif comp == 'A+1':
+		acomp = '0'
+		comp = '110111'
+	elif comp == 'M+1':
+		acomp = '1'
+		comp = '110111'
+	elif comp == 'D-1':
+		acomp = '0'
+		comp = '001110'
+	elif comp == 'A-1':
+		acomp = '0'
+		comp = '110010'
+	elif comp =='M-1':
+		acomp = '1'
+		comp = '110010'
+	elif comp == 'D+A':
+		acomp = '0'
+		comp = '000010'
+	elif comp == 'D+M':
+		acomp = '1'
+		comp = '000010'				
+	elif comp == 'D-A':
+		acomp = '0'
+		comp = '010011'
+	elif comp == 'D-M':
+		acomp = '1'
+		comp = '010011'
+	elif comp == 'A-D':
+		acomp = '0'
+		comp = '000111'
+	elif comp == 'M-D':
+		acomp = '1'
+		comp = '000111'
+	elif comp == 'D&A':
+		acomp = '0'
+		comp = '000000'
+	elif comp == 'D&M':
+		acomp = '1'
+		comp = '000000'
+	elif comp == 'D|A':
+		acomp = '0'
+		comp = '010101'
+	elif comp == 'D|M':
+		acomp = '1'
+		comp = '010101'
+
+	comptranslated = acomp + comp
+	return comptranslated
+
+def desttrans(dest):
+
+	if dest == None:
+		dest = '000'
+	elif dest == 'M':
+		dest = '001'
+	elif dest == 'D':
+		dest = '010'
+	elif dest == 'MD':
+		dest = '011'
+	elif dest == 'A':
+		dest = '100'
+	elif dest == 'AM':
+		dest = '101'
+	elif dest == 'AD':
+		dest = '110'
+	elif dest == 'AMD':
+		dest = '111'
+
+	return dest 
+
+def jumptrans(jump):
+
+	if jump == None:
+		jump = '000'
+	elif jump == 'JGT':
+		jump = '001'
+	elif jump == 'JEQ':
+		jump = '010'
+	elif jump == 'JGE':
+		jump = '011'
+	elif jump == 'JLT':
+		jump = '100'
+	elif jump == 'JNE':
+		jump = '101'
+	elif jump == 'JLE':
+		jump = '110'
+	elif jump == 'JMP':
+		jump = '111'
+
+	return jump
